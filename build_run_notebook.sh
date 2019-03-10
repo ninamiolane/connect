e9869aec812559fb8ba2536d73478ebedc943ca4
Nina Miolane <ninamio78@gmail.com>
2019-03-10 03:36:10 +0100

    Clean main repo

--- build_run_notebook.sh
+++ build_run_notebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nvidia-docker build --no-cache -f Dockerfile.jupyter . -t sommet/notebook
+nvidia-docker build -f docker/Dockerfile.jupyter . -t sommet/notebook
 nvidia-docker run -p8888:8888 -it -v/scratch/:/scratch -v/home/nina:/home/nina sommet/notebook
